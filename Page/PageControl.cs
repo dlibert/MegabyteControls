377a50a55038e7d1a24a07f7dfef5e57125fa50f
Delsaut Pierre-Arnaud <diplomegalo@hotmail.com>
2012-04-11 11:34:59 +0200

    Test conflit pad

--- Page/PageControl.cs
+++ Page/PageControl.cs
@@ -19,7 +19,7 @@ namespace Megabyte.Web.Controls.Page {
     /// TODO: Update summary.
     /// </summary>
     public class PageControl : System.Web.UI.Page, Interfaces.IWebControl {
-        //plop3
+        //plop3 
         public T GetDB<T>() {
             return Megabyte.Web.Controls.Helper.MegabyteHelper.GetDB<T>();
         }
